7451e591d59108897d8991a1015812912a3c4b6b
yzh <yzh@ivu.de>
2024-07-02 10:18:40 +0200

    BWS18067: add files

--- blog_comment/urls.py
+++ blog_comment/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'blog_comment'
 urlpatterns = [
-    re_path(r'^/post/(?P<post_pk>[0-9]+)/$', views.post_comment, name='post_comment'),
+    re_path(r'^post/(?P<post_pk>[0-9]+)/$', views.post_comment, name='post_comment'),
 ]
